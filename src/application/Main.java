dc695d33fa8bad7167238a7eeeb703d8e03b8833
Danilson Sanches <dsanches06@outlook.com>
2020-03-05 15:55:17 +0000

    Program created

---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.text.ParseException;
+
+public class Main {
+
+	public static void main(String[] args) throws ParseException {
+		
+		/**
+		 * 
+		 * INTERFACE 
+		 * 
+		 * É um tipo que define um conjunto de operações que uma 
+		 * classe deve implementar, ou seja, interface estabelece
+		 * um contrato que a clase deve cumprir.
+		 * 
+		 * Sintaxe
+		 * 
+		 * atraves da palavra reservada "interface"
+		 * 
+		 * 
+		 *   interface Shape() {
+		 *       double area();
+		 *       double perimeter();
+		 *   }
+		 *   
+		 *   basicamente, é para criar sistemas com baixo acomplamento 
+		 *   e flexiveis
+		 * 
+		 */
+	
+	}
+
+}
